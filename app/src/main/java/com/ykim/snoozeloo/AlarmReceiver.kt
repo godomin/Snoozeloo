1d8ebd92e4a3ad9e25b0519fda53a66e0a799cc2
godomin <ykim000514@gmail.com>
2024-11-20 00:05:48 +0900

    Show activity directly from broadcast receiver

--- app/src/main/java/com/ykim/snoozeloo/AlarmReceiver.kt
+++ app/src/main/java/com/ykim/snoozeloo/AlarmReceiver.kt
@@ -3,12 +3,18 @@ package com.ykim.snoozeloo
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.ykim.snoozeloo.presentation.TriggerActivity
 import com.ykim.snoozeloo.presentation.util.showNotificationWithFullScreenIntent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        context?.showNotificationWithFullScreenIntent(intent)
+        //context?.showNotificationWithFullScreenIntent(intent)
+        val newIntent = Intent(context, TriggerActivity::class.java).apply {
+            intent?.extras?.let { putExtras(it) }
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        context?.startActivity(newIntent)
     }
 }
